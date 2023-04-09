7d683b4839a3d287664662b70e36c5f47b7a7b1a
kumaran_virat <86183777+geekykumaran@users.noreply.github.com>
2023-04-09 15:18:31 +0530

    final

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require("express")
 const app = express() 
 
+app.get("/",(req,res)=>{
+	return res.json({"message":"hello world"}) 
+});
+
 app.listen(3000,(err)=>{
 	if(err) console.log("error starting")
 	else console.log("started at 3000")
